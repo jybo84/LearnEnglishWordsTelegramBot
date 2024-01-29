bcc1f601e39d26f3cfc6b11bc33b96d2bc783b9a
jybo84 <135285790+jybo84@users.noreply.github.com>
2024-01-29 10:39:09 +1000

    Revert "ConsoleTrainer логика кнопки 1 (учить слова)"

--- src/main/kotlin/TelegramBot/ConsoleTrainer.kt
+++ src/main/kotlin/TelegramBot/ConsoleTrainer.kt
@@ -3,18 +3,24 @@ package TelegramBot
 import java.io.File
 import kotlin.math.roundToInt
 
+
 const val LIMIT = 3
 fun main() {
 
     val text = File("words.txt")
+    text.writeText("hello |привет |5")
+    text.appendText("\ndog |собака |5")
+    text.appendText("\ncat |кошка |2")
 
     val dictionary = mutableListOf<Word>()
+
     val lines = text.readLines()
     for (el in lines) {
         val splitString = el.split(" |")
         val word = Word(splitString[0].trim(), splitString[1].trim(), splitString[2].toInt())
         dictionary.add(word)
     }
+
     println(
         """
             МЕНЮ:
@@ -23,40 +29,19 @@ fun main() {
         0 - Выход 
     """.trimIndent()
     )
+
     while (true) {
         println("Выберите пункт меню")
-        try {
-            val userNumber = readln().toInt()
-            when (userNumber) {
-                1 -> {
-                    val remainsWord = dictionary.filter { it.correctAnswersCount < LIMIT }
-                    if (remainsWord.isEmpty())
-                        println("Выучены все слова")
-                    else {
-                        do {
-                            val listOriginal = remainsWord.map { it.engWord }
-                            println((listOriginal.random().uppercase()))
-                            println("Выберите вариант ответа из списка: ")
-                            remainsWord.forEachIndexed { ind, el ->
-                                println("${ind + 1} - ${el.rusWord} ")
-                            }
-                            println()
-                            println("0 - выйти в меню")
-                            val user = readln().toInt()
-                        } while (user != 0)
-                    }
-                }
+        val userNumber = readln().toInt()
 
-                2 -> {
-                    val learnWord = dictionary.filter { it.correctAnswersCount >= LIMIT }.size
-                    println("$learnWord из ${dictionary.size} | ${((learnWord.toFloat() / dictionary.size) * 100).roundToInt()}%")
-                }
-
-                0 -> break
-                else -> println("Вы ввели некоректное число")
+        when (userNumber) {
+            1 -> TODO()
+            2 -> {
+                val learnWord = dictionary.filter { it.correctAnswersCount >= LIMIT }.size
+                println("$learnWord из ${dictionary.size} | ${((learnWord.toFloat() / dictionary.size) * 100).roundToInt()}%")
             }
-        } catch (e: NumberFormatException) {
-            println("Введен неправильый формат числа")
+            0 -> break
+            else -> println("Вы ввели некорекное число")
         }
     }
 }
