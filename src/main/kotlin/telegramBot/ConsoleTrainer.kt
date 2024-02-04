b5ead1fb9b3964b425e30c235e54d79426bbc188
Maxim <jybo@mail.ru>
2024-02-04 23:20:07 +1000

    ConsoleTrainer - refactoring

--- src/main/kotlin/telegramBot/ConsoleTrainer.kt
+++ src/main/kotlin/telegramBot/ConsoleTrainer.kt
@@ -1,8 +1,138 @@
 package telegramBot
 
 import java.io.File
-import kotlin.math.roundToInt
 
+const val LIMIT_OF_LEARNED_WORD = 1
+const val MAX_LIST_WORD_FOR_USER = 4
+
+fun main() {
+    val trainer = LearnWordsTrainer()
+
+    while (true) {
+        println(
+            """
+
+            МЕНЮ:
+        1 - Учить слова
+        2 - Статистика
+        
+        0 - Выход
+    """.trimIndent()
+        )
+        when (readln().toIntOrNull()) {
+            1 -> {
+                while (true) {
+                    val question = trainer.getNextQuestion()
+                    if (question == null) {
+                        println("Все слова выучены")
+                        break
+                    } else {
+                        println(question.questionToString())
+                        val userAnswerInput = readln().toIntOrNull()
+                        if (userAnswerInput == 0) break
+                        if (trainer.checkAnswer(userAnswerInput?.minus(1))) {
+                            println("Правильно\n")
+                        } else {
+                            println("Неправильно ${question.correctAnswer.questionsWord} -это ${question.correctAnswer}")
+                        }
+                    }
+                }
+            }
+
+            2 -> {
+                val statistic = trainer.getStatistics()
+                println("Выучено ${statistic.learned} из ${statistic.total} слов | ${statistic.percent}%")
+            }
+
+            0 -> break
+
+            else -> println("Введите 1, 2 или 0")
+        }
+    }
+}
+
+data class Word(
+    val questionsWord: String,
+    val translate: String,
+    var correctAnswerCount: Int = 0,
+)
+
+fun Question.questionToString(): String {
+    val variants = this.variants
+        .mapIndexed { index: Int, word: Word -> "${index + 1} - ${word.translate}" }
+        .joinToString(separator = "\n")
+    return this.correctAnswer.questionsWord + "\n" + variants + "\n0 - выйти в меню"
+}
+
+data class Statistics(
+    val learned: Int,
+    val total: Int,
+    val percent: Int,
+)
+
+data class Question(
+    val variants: List<Word>,
+    val correctAnswer: Word,
+)
+
+
+class LearnWordsTrainer {
+    private var question: Question? = null
+    private var dictionary = loadDictionary()
+
+    private fun loadDictionary(): List<Word> {
+        val dictionary = mutableListOf<Word>()
+        val wordFile = File("words.txt")
+        wordFile.readLines().forEach {
+            val splitLine = it.split("|")
+            dictionary.add(Word(splitLine[0], splitLine[1], splitLine[2].toIntOrNull() ?: 0))
+        }
+        return dictionary
+    }
+
+    private fun saveDictionary(words: List<Word>) {
+        val wordsFile = File("words.txt")
+        wordsFile.writeText("")
+        for (word in words) {
+            wordsFile.appendText("${word.questionsWord}| ${word.translate}|${word.correctAnswerCount}")
+        }
+    }
+
+    fun getStatistics(): Statistics {
+        val learned = dictionary.filter { it.correctAnswerCount >= LIMIT_OF_LEARNED_WORD }.size
+        val total = dictionary.size
+        val percent = learned * 100 / total
+        return Statistics(learned, total, percent)
+    }
+
+    fun getNextQuestion(): Question? {
+        val notLearnedList = dictionary.filter { it.correctAnswerCount < LIMIT_OF_LEARNED_WORD }
+        if (notLearnedList.isEmpty()) return null
+        val questionWords = notLearnedList.shuffled().take(MAX_LIST_WORD_FOR_USER)
+        val correctAnswer = questionWords.random()
+        question = Question(
+            variants = questionWords,
+            correctAnswer = correctAnswer,
+        )
+        return question
+    }
+
+    fun checkAnswer(userAnswerIndex: Int?): Boolean {
+        return question?.let {
+            val correctAnswerId = it.variants.indexOf(it.correctAnswer)
+            if (correctAnswerId == userAnswerIndex) {
+                it.correctAnswer.correctAnswerCount++
+                saveDictionary(dictionary)
+                true
+            } else {
+                false
+            }
+        } ?: false
+    }
+}
+
+
+/*
 const val LIMIT_OF_LEARNED_WORD = 1
 const val MAX_LIST_WORD_FOR_USER = 4
 
@@ -12,7 +142,7 @@ fun main() {
     while (true) {
         println(
             """
-                
+
             МЕНЮ:
         1 - Учить слова
         2 - Статистика
@@ -118,4 +248,5 @@ class LearnWordTrainer() {
         }
     }
 }
+ */
 
